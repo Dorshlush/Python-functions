5ff86c2c1e488b9ccbf6b49d8edba81c1b5c39d3
Dorshlush <dor.shlush@gmail.com>
2023-04-21 20:30:34 +0300

    deleting generate txt function

--- functions.py
+++ functions.py
@@ -4,10 +4,4 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 return summarizer(text)
 
 
-from transformers import pipeline, set_seed
-
-def generate_text(text)
-generator = pipeline('text-generation', model='facebook/bart-large-cnn')
-set_seed(42)
-return generator(text)
 
