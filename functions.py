a5cd3728a616587c0dbcdfe8c9b2eb16368de667
Dorshlush <dor.shlush@gmail.com>
2023-04-23 10:49:48 +0300

    ads

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
 from transformers import pipeline
-def summarize_text(text)
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-return summarizer(text)
 
-
-
+def summarize_text(event, context):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    text = event['body']
+    summary = summarizer(text)
+    return {
+        'statusCode': 200,
+        'body': summary[0]['summary_text']
+    }
